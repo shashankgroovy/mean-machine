e639843e479ca513a955b70741ebdb0b96395f21
Shashank Srivastav <shashankgrovy@gmail.com>
2015-03-18 02:02:52 +0530

    express way server

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 "use-strict"
 
 // Method 1
-// pure nodejs way
+// expressjs way
 
-var http = require('http'),
-    fs = require('fs');
+var express = require('express'),
+    app = express(),
+    path = require('path');
 
-http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Access-Control-Allow-Origin' : '*'
-  });
+app.get('/' ,function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
 
-  // grab index.html
-  var readStream = fs.createReadStream(__dirname + '/index.html');
-
-  readStream.pipe(res);
-}).listen(3000);
+app.listen(3000);
 
 console.log('Listening at http://localhost:3000');
