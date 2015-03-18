18f39261aa12392796a60a513a0d8231cc099a07
Shashank Srivastav <shashankgrovy@gmail.com>
2015-03-18 12:34:23 +0530

    use expressRouter() to channel requests

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ app.get('/' ,function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(3000);
+// using the express Router instance to channel our requests
+var adminRouter = express.Router();
+adminRouter.get('/', function(req, res) {
+  res.send('dashboard');
+});
 
+adminRouter.get('/users', function(req, res) {
+  res.send('users page');
+});
+
+adminRouter.get('/posts', function(req, res) {
+  res.send('posts page');
+});
+
+// apply all the routes back to our application
+app.use('/admin', adminRouter);
+
+app.listen(3000);
 console.log('Listening at http://localhost:3000');
