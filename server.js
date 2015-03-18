41a6081a56587c6ebf54683478c806681f73b585
Shashank Srivastav <shashankgrovy@gmail.com>
2015-03-18 13:47:09 +0530

    add parameters to url routes and use params()to intercept and validate

--- server.js
+++ server.js
@@ -13,12 +13,33 @@ app.get('/' ,function(req, res) {
 
 // using the express Router instance to channel our requests
 var adminRouter = express.Router();
+
+/* router.use() -
+ * A route middleware that will execute something before a request is processed.
+ */
+adminRouter.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
+
+// define the routes
 adminRouter.get('/', function(req, res) {
   res.send('dashboard');
 });
 
 adminRouter.get('/users', function(req, res) {
   res.send('users page');
+});
+
+// add a validation check before GET request is executed
+adminRouter.param('name', function(req, res, next, name) {
+  console.log('Running validations on', name);
+  req.name == name;
+  next();
+});
+
+adminRouter.get('/users/:name', function(req, res) {
+  res.send('hello ' + req.params.name + '!');
 });
 
 adminRouter.get('/posts', function(req, res) {
