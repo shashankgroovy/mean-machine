54afdd51b0ed19aab4c880ab60e2b3ec4cbf2b1e
Shashank Srivastav <shashankgrovy@gmail.com>
2015-03-19 16:20:53 +0530

    add user authentication

---
+++ models/user.js
@@ -0,0 +1,34 @@
+"use-strict"
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+mongoose.connect('mongodb://fnaticshank:shashank@ds063160.mongolab.com:63160/mongonode')
+
+
+// user schema
+var UserSchema = new Schema({
+  name: String,
+  username: {type: String, required: true, index: {unique: true}},
+  password: {type: String, required: true, select: false }
+});
+
+// hash the password
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
+});
+
+UserSchema.methods.comparePassword = function(password) {
+  var user = this;
+  return bcrypt.compareSync(password, user.password);
+};
+
+// return the model
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var express = require('express');
 var app  = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var mongoose = require('mongoose');
+var path = require('path');
 var port = process.env.PORT || 3000;
+
+var User = require('./models/user');
+
 
 // use body-parser to grab information from POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,15 +24,81 @@ app.use(function(req, res, next) {
 // log all requests to the console
 app.use(morgan('dev'));
 
-app.get('/', function(req, res) {
-  res.send('Dashboard HomePage');
+// Routes API
+var apiRouter = express.Router();
+
+apiRouter.use(function(req, res, next) {
+  console.log('This is a RESTful api');
+  next();
 });
 
-var apiRouter = express.Router();
 apiRouter.get('/', function(req, res) {
   res.json({ message: 'welcome to the api' });
 });
 
+apiRouter.route('/users')
+  .post(function(req, res) {
+    var user = new User();
+
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+    
+    user.save(function(err) {
+      if (err) {
+        if (err.code == 11000)
+          return res.json({ success: false, message: 'Username already exists'});
+        else
+          return res.send(err);
+      }
+      res.json({message: 'User created!'});
+    });
+  })
+  .get(function(req, res) {
+    User.find(function(err, users) {
+      if(err) res.send(err);
+      res.json(users);
+    });
+  });
+
+apiRouter.route('/users/:user_id')
+  .get(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if(err) res.send(err);
+      res.json(user);
+    });
+  })
+  .put(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) res.send(err);
+      if (req.body.name) user.name = req.body.name;
+      if (req.body.username) user.username = req.body.username;
+      if (req.body.password) user.password = req.body.password;
+      
+      //save the user
+      user.save(function(err) {
+        if (err) res.send(err);
+        res.json({message: 'User info updated'});
+      });
+    });
+  })
+  .delete(function(req, res) {
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user) {
+      if (err) return res.send(err);
+      res.json({ message: 'successfully deleted'});
+    });
+  });
+
+
+// Universal routes
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// register our custom router instances back to the application
 app.use('/api', apiRouter);
+
 app.listen(port);
 console.log('Listening on port ' + port);
